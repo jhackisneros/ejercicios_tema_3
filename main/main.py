d8aecef08d65598fbcd18cc90a89ff2d076836e2
zair <oliveracisneros1@gmail.com>
2025-04-03 22:03:04 +0000

    cambios

--- main/main.py
+++ main/main.py
@@ -1,14 +1,14 @@
 def hanoi(n, source, target, auxiliary):
     if n == 1:
-        print(f"Mover disco de {source} a {target}")
+        print(f"Mover piedra de {source} a {target}")
         return
     hanoi(n - 1, source, auxiliary, target)
-    print(f"Mover disco de {source} a {target}")
+    print(f"Mover piedra de {source} a {target}")
     hanoi(n - 1, auxiliary, target, source)
 
 def main():
-    n = 3  # Número de discos
-    hanoi(n, 'A', 'C', 'B')
+    n = 74  # Número de piedras
+    hanoi(n, 'Columna A', 'Columna C', 'Columna B')
 
 if __name__ == "__main__":
     main()
